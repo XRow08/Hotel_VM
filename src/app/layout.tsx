a5e62711b13a772d1416d17e916d7ba6a0e690d9
XRow08 <murilloaugustoe081@gmail.com>
2024-06-03 14:27:13 -0300

    fat: googl

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,16 +29,9 @@ export default function RootLayout({
       <head>
         <script
           async
-          id="dataLayer"
-          dangerouslySetInnerHTML={{
-            __html: `
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-TJ3JW83G');`,
-          }}
+          src="https://www.googletagmanager.com/gtag/js?id=AW-16584454229"
         />
+        <script async src="/google.js" />
       </head>
       <body className={`${zilla.variable} ${montserrat.variable}`}>
         <noscript>
